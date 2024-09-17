2e245728d5b1dd4a44aaceddc44ea873ab64ae08
NolanReffett <155671878+NolanReffett@users.noreply.github.com>
2024-09-17 12:06:18 -0500

    nextlevel

--- NextLevel.java
+++ NextLevel.java
@@ -1,20 +1,22 @@
+import mayflower.*;
 
-/**
- * Write a description of class NextLevel here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class NextLevel
+public class NextLevel extends World
 {
     // instance variables - replace the example below with your own
     
-
+    World nextWorld;
     /**
      * Constructor for objects of class NextLevel
      */
-    public NextLevel()
+    public NextLevel(World nextWorld)
     {
-        
+        System.out.print("Loading next World, enter to start");
+        this.nextWorld = nextWorld;
+    }
+    
+    public void act() 
+    {
+        if(Mayflower.isKeyDown(Keyboard.KEY_ENTER))
+            Mayflower.setWorld(nextWorld);
     }
 }
