587b9d1a576fb93282722caade3e1428b494d151
NolanReffett <155671878+NolanReffett@users.noreply.github.com>
2024-09-23 11:36:01 -0500

    empty stuff

--- Ladder.java
+++ Ladder.java
@@ -1,20 +1,17 @@
 import mayflower.*;
 /**
-<<<<<<< HEAD
  * When the player comes in contact with this object they can ignore normal gravity and climb vertically
  * (without gravity affecting their speed)
  *
-=======
  * 
  * Allows Cat to climb upwards without jumping
  * 
->>>>>>> 3703fbc1dee8168dd1bf4747b836738116fb3e27
  */
 public class Ladder extends Actor
 {
     public Ladder()
     {
-        
+       //setBounds(0,0,40,100); 
     }
 
     public void act()
