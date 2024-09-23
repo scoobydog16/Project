aea2e5162250e3a0f0feb781a18341aa14f19d2e
yash <connect2yash15@gmail.com>
2024-09-23 12:18:36 -0500

    bomb appears score

--- MyWorld.java
+++ MyWorld.java
@@ -1,39 +1,48 @@
 import mayflower.*;
 
+
+
 public class MyWorld extends World 
 {
 
-	private Cat cat;
-	private Dog dog;
-	private Jack jack;
-	private Ninja ninja;
-	private Block blockA;
-	private Block blockB;
-	
+    private Cat cat;
+    private Dog dog;
+    private Jack jack;
+    private Ninja ninja;
+    private Block blockA;
+    private Block blockB;
+    private Bomb bomb;
+    
+    
+    
     public MyWorld() 
     {
-    	setBackground("img/BG/BG.png");
-    	
-    	cat = new Cat();
-    	addObject(cat, 400, 0);
-    	
-    	dog = new Dog();
-    	//addObject(dog, 200, 100);
-    	
-    	jack = new Jack();
-    	//addObject(jack, 300, 100);
-    	
-    	ninja = new Ninja();
-    	//addObject(ninja, 400,100);
-    	
-    	blockA = new Block();
-    	addObject(blockA, 400,500);
-    	blockB = new Block();
-    	addObject(blockB, 528,372);
-    	
-    	Mayflower.showBounds(true);
-    	
-    	showText("Lives: " + cat.getLives(), 10, 30, Color.BLACK);
+        setBackground("img/BG/BG.png");
+        
+        bomb = new Bomb();
+        addObject(bomb, 399, 200);
+        
+        
+        cat = new Cat();
+        addObject(cat, 400, 0);
+        
+        dog = new Dog();
+        //addObject(dog, 200, 100);
+        
+        jack = new Jack();
+        //addObject(jack, 300, 100);
+        
+        ninja = new Ninja();
+        //addObject(ninja, 400,100);
+        
+        blockA = new Block();
+        addObject(blockA, 400,500);
+        blockB = new Block();
+        addObject(blockB, 528,372);
+        
+        Mayflower.showBounds(true);
+        
+        showText("Lives: " + cat.getLives(), 10, 30, Color.BLACK);
     }
     
     public void act()
