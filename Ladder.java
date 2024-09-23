3373313264310e0a0f97f3b576a7ddbde9931eb5
samarthwashere <samjapan2078@gmail.com>
2024-09-23 11:28:47 -0500

    flying bomb images

--- Ladder.java
+++ Ladder.java
@@ -1,27 +1,16 @@
 import mayflower.*;
 /**
- * Write a description of class Ladder here.
- *
- * @author (your name)
- * @version (a version number or a date)
+ * 
+ * Allows Cat to climb upwards without jumping
+ * 
  */
 public class Ladder extends Actor
 {
-
-    /**
-     * Constructor for objects of class Ladder
-     */
     public Ladder()
     {
-        // setImage...
+        
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
     public void act()
     {
         
