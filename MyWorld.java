e7ed6e82f9e9fe35f72ce8a59007ad241fa91df6
NolanReffett <155671878+NolanReffett@users.noreply.github.com>
2024-09-25 11:00:02 -0500

    stuffffff

--- MyWorld.java
+++ MyWorld.java
@@ -4,8 +4,6 @@ import mayflower.*;
 
 public class MyWorld extends World 
 {
-
-<<<<<<< HEAD
     private Cat cat;
     private Dog dog;
     private Jack jack;
@@ -13,6 +11,7 @@ public class MyWorld extends World
     private Block blockA;
     private Block blockB;
     private Bomb bomb;
+    	private Ladder ladder;
     
     
     
@@ -44,43 +43,7 @@ public class MyWorld extends World
         Mayflower.showBounds(true);
         
         showText("Lives: " + cat.getLives(), 10, 30, Color.BLACK);
-=======
-	private Cat cat;
-	private Dog dog;
-	private Jack jack;
-	private Ninja ninja;
-	private Block blockA;
-	private Block blockB;
-	private Ladder ladder;
-	
-    public MyWorld() 
-    {
-    	setBackground("img/BG/BG.png");
-    	
-    	cat = new Cat();
-    	addObject(cat, 400, 0);
-    	
-    	dog = new Dog();
-    	//addObject(dog, 200, 100);
-    	
-    	jack = new Jack();
-    	//addObject(jack, 300, 100);
-    	
-    	ninja = new Ninja();
-    	//addObject(ninja, 400,100);
-    	
-    	ladder = new Ladder();
-    	addObject(ladder, 458,372);
-    	
-    	blockA = new Block();
-    	addObject(blockA, 400,500);
-    	blockB = new Block();
-    	addObject(blockB, 528,372);
-    	
-    	Mayflower.showBounds(true);
-    	
-    	showText("Lives: " + cat.getLives(), 10, 30, Color.BLACK);
->>>>>>> d23e7589d5f1e6ba9d6bc039a0a53feb0d737e7d
+
     }
     
     public void act()
